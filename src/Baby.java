5e34947503ed7bf5f8fc943f752f4d315059f542
ngocuong <ngo.cuong@abv.bg>
2021-10-13 22:46:36 +0300

    clean

--- src/Baby.java
+++ src/Baby.java
@@ -1,37 +1,4 @@
 package src;
-// • Class names are Capitalized
-// • 1 Class = 1 file
-// • Having a main method means the class can be run
-// JAVA Types 
-// • Primitive types are basic java types
-// – int, long, double, boolean, char, short, byte, float
-// – The actual values are stored in the variable
-
-// • Reference types are arrays and objects
-// – String, int[], Baby, ...
-
-// How java stores primitives
-// • Variables are like fixed size cups
-// • Primitives are small enough that they just fit
-// into the cup
-
-// How java stores objects
-// • Objects are too big to fit in a variable (too big for the cup)
-// – Stored somewhere else
-// – Variable stores a number that locates the object
-// • The object’s location is called a reference
-
-// Static and non Static methods
-// • Applies to fields and methods !!!!!!
-// • Means the field/method
-// – Is defined for the class declaration,
-// – Is not unique for each instance
-// Java main() method is always static, so 
-// that compiler can call it without the creation of an 
-// object or before the creation of an object of the class. 
-// In any Java program, the main() method is the starting point 
-// from where compiler starts program execution. 
-// So, the compiler needs to call the main() method.
 
 public class Baby {
     // Fields (setting their type)
